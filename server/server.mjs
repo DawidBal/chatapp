7dd63923f33222d2b536789302f75ccb32aafcc0
Dawid <baluszynskidawid@gmail.com>
2021-06-24 15:35:42 +0200

    remove unused code

--- server/server.mjs
+++ server/server.mjs
@@ -1,7 +1,6 @@
 import { Server } from 'Socket.io';
 import express from 'express';
 import http from 'http';
-import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
@@ -9,16 +8,14 @@ const PORT = 3001;
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
   }
 });
-const __dirname = path.resolve();
 
 server.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
 })
 
-app.use(express.static(path.join(__dirname, '.')));
+io.on('connection', (client) => {
 
 io.on('connection', (client) => {
   console.log("A user has connected");
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
-import openSocket from 'socket.io-client';
 import Chat from './Components/Chat';
 import UsernameForm from './Components/UsernameForm';
-
-const socket = openSocket('http://localhost:3001');
 
 const App = () => {
 
   const [userName, setUsername] = useState("");
   const [isUsernameSet, setisUsernameSet] = useState(false);
-
-  socket.on('connected', (msg: string) => {
-    console.log(msg);
-  })
 
   return (
     <>
