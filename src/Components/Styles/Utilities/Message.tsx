9dd25786ca7288548c3507e1fa2d7c7e7762079c
Dawid <baluszynskidawid@gmail.com>
2021-06-28 17:40:13 +0200

    refactor: move message styled components to separate file

---
+++ src/Components/Styles/Utilities/Message.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+type StyledProps = {
+  right?: boolean
+}
+
+export const MessageContent = styled.div`
+
+`
+
+export const MessageText = styled.p`
+  margin: 0;
+  padding: 0.4rem 0.6rem;
+  background-color: #0064d6;;
+  color: #fff;
+  border-radius: 25px;
+  font-size: 1rem;
+  display: inline;
+`
+
+export const MessagePerson = styled.span<StyledProps>`
+  ${({ right }) => right ? 'padding-right: 0.6rem' : 'padding-left: 0.6rem;'};
+  font-size: 0.8rem;
+  color: #0000008f;
+  display: block;
+  margin: 0.5rem 0;
+`
--- src/Components/Utilities/Message.tsx
+++ src/Components/Utilities/Message.tsx
@@ -1,32 +1,7 @@
 import React from 'react'
-import styled from 'styled-components';
 import { Message as Msg } from '../Styles/Chat/Chat';
+import { MessageContent, MessageText, MessagePerson } from '../Styles/Utilities/Message';
 
-const MessageContent = styled.div`
-
-`
-
-const MessageText = styled.p`
-  margin: 0;
-  padding: 0.4rem 0.6rem;
-  background-color: #000000;;
-  color: #fff;
-  border-radius: 25px;
-  font-size: 1rem;
-  display: inline;
-`
-
-const MessagePerson = styled.span<StyledProps>`
-  ${({ right }) => right ? 'padding-right: 0.6rem' : 'padding-left: 0.6rem;'};
-  font-size: 0.8rem;
-  color: #0000008f;
-  display: block;
-  margin: 0.5rem 0;
-`
-
-type StyledProps = {
-  right?: boolean
-}
 
 type Props = {
   message: string,
